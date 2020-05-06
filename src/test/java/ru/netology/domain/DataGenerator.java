d8cfff638a07b6a5642333ecab03bcbc6e69469f
Anna Popova <anna.popova201909@gmail.com>
2020-05-06 13:39:37 +0300

    finished ex 1

---
+++ src/test/java/ru/netology/domain/DataGenerator.java
@@ -0,0 +1,24 @@
+package ru.netology.domain;
+
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationByCardInfo generateByCard(String locale) {
+            Faker faker = new Faker(new Locale("ru"));
+            return new RegistrationByCardInfo(
+                    faker.address().city(),
+                    faker.name().fullName(),
+                    faker.phoneNumber()
+            );
+        }
+    }
+}
